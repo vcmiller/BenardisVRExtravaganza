9192a31bcd5387264669e7e250ddd27b3c1b13e7
Sean Briggs <seanrgbriggs@gmail.com>
2016-11-19 02:32:38 -0500

    so much stuff man its 3 am i dont care anymroe

---
+++ Assets/Scripts/Arrow/ArrowHand.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowHand : MonoBehaviour {
+
+    public bool grabbing;
+    public Transform toGrab;
+
+	// Use this for initialization
+	void Start () {
+        toGrab = null;
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<ArrowBase>())
+        {
+            if (grabbing)
+            {
+                toGrab = col.transform;
+                col.transform.SetParent(transform);
+                col.transform.localPosition = Vector3.zero;
+                col.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Arrow/Bow.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bow : MonoBehaviour {
+
+    public Vector3 notch;
+    public float maxArrowDistance;
+    public float maxVelocity;
+    ArrowBase nocked;
+    ArrowHand hand;
+
+    bool lastGrabState;
+
+    // Use this for initialization
+    void Start () {
+        hand = FindObjectOfType<ArrowHand>();
+        lastGrabState = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (nocked)
+        {
+            if (Vector3.Distance(notch, nocked.transform.position) > maxArrowDistance)
+            {
+                nocked.transform.position = (nocked.transform.position - notch).normalized * maxArrowDistance;
+            }
+            nocked.transform.rotation = Quaternion.LookRotation(notch - nocked.transform.position);
+        }
+        
+	}
+
+    void NockReleaseSpinLock()
+    {
+        if(hand.grabbing != lastGrabState)
+        {
+            if (lastGrabState) {
+                Nock(hand.toGrab.GetComponent<ArrowBase>());
+            } else {
+                Release();
+            }
+
+            lastGrabState = !lastGrabState;
+        }
+    }
+
+    void Nock(ArrowBase ab)
+    {
+        nocked = ab;
+    }
+
+    void Release()
+    {
+        nocked.GetComponent<Rigidbody>().velocity = transform.forward * maxVelocity * (Vector3.Distance(notch, nocked.transform.position) / maxArrowDistance);
+    }
+}
